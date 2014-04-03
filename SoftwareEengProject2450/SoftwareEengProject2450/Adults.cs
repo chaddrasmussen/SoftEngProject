58500fa50b1d6a9d5279900f79daab272ddb137a
JAcKDuB <JonWarner11@gmail.com>
2014-04-02 18:57:47 -0600

    Rest of the media
    Missed the adult class

---
+++ SoftwareEengProject2450/SoftwareEengProject2450/Adults.cs
@@ -0,0 +1,21 @@
+﻿namespace Library
+{
+	sealed class Adult : MediaObject
+	{
+		// ********************************* Constants ***************************************
+
+		private const uint ADULT_LOAN_TIME = 14;
+
+		// ******************************* Constructors **************************************
+
+		private Adult() { }
+
+		public Adult(string title, uint numberOfCopies)
+			: base(title, ADULT_LOAN_TIME, numberOfCopies)
+		{
+
+		}
+
+		// ***********************************************************************************
+	}
+}
