84f38c87f613a82b4bce0bfd16b5f7a4485b310a
Nikki Talley <hkncox@gmail.com>
2014-04-15 16:26:25 -0600

    removed duplicate

--- SoftwareEengProject2450/SoftwareEengProject2450/Form1.cs
+++ SoftwareEengProject2450/SoftwareEengProject2450/Form1.cs
@@ -328,8 +328,7 @@ namespace Library
                     mediaSD.Remove(i);
                     UpdateScreens();
                     ClearRemoveMediaFields();
-					UpdateScreens();
-					ClearRemoveMediaFields();
+				
                 }
             }
         }
@@ -574,10 +573,14 @@ namespace Library
             {
                 txtPatronItemsCheckedOut.Items.Clear();
             }
-
-            displayMedia();
-            displayPatrons();
-
+            if (mediaSD.Count >0)
+            {
+                displayMedia();
+            }
+            if (patronSD.Count > 0)
+            {
+                displayPatrons();
+            }
             if (txtDisplayPatron.SelectedItems.Count == 0)
             {
                 btnCheckOut.Enabled = false;
